91f8f615456519164a9ea28ab043b332b480ff61
sudikshyakoju <sudikoju@gmail.com>
2021-02-24 15:41:30 +0545

    labwork

---
+++ lab4/pythonlab/labwork.py
@@ -0,0 +1,6 @@
+students=int(input('enter number of students:'))
+apples=int(input('enter number of apples:'))
+d=(apples//students)
+r=(apples%students)
+print(f"each students {d}")
+print(f"the remaining apples are {r}")
