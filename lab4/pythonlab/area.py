0613e14938873d12515d6d3e41c5c402860ffaa5
sudikshyakoju <sudikoju@gmail.com>
2021-03-02 09:33:36 +0545

    terminal

---
+++ lab4/pythonlab/area.py
@@ -0,0 +1,5 @@
+#WAP that reads length of base and height of right-angled triangle and print area
+
+length=int(input('enter length:'))
+height=int(input('enter height'))
+area=(length*)
---
+++ lab4/pythonlab/sum.py
@@ -0,0 +1,7 @@
+#WAP that take three inputs and print their sum.
+
+value1=int(input('enter value1:'))
+value2=int(input('enter value2:'))
+value3=int(input('enter value3:'))
+sum=value1+value2+value3
+print(sum)
