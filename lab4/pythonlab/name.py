dff2f32b4154f49abc814b9b810dd2323949ff71
sudikshyakoju <sudikoju@gmail.com>
2021-03-17 11:56:33 +0545

    dgdgfdgfd

--- lab4/pythonlab/name.py
+++
@@ -1,7 +0,0 @@
-name=input('enter your name:')
-age=int(input('enter your age:'))
-print(f'My name is {name} and my age is {age}.')
-print("My name is" ,name, "and my age is" ,age,)
-print("My name is %s and my age is %d" % (name,age))
-print('My name is' +name+ 'and my age is' +str(age)+)
-print("My name is {} and my age is {}".format(name, age))
